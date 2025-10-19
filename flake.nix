e7ca64f954c4ae0593ef64fcf1fb929a22e813d8
Baitinq <manuelpalenzuelamerino@gmail.com>
2025-10-19 17:35:42 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "LaunchProbe Devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            python3
+            python3Packages.uv
+          ];
+        };
+      }
+    );
+}
